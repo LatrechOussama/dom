058fa82575cd5190fd51b7d2fe9e13f484fab25f
LatrechOussama <boudy.lat@gmail.com>
2021-12-13 02:53:39 +0100

    DomWS

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 const btnaddtag = document.getElementsByClassName("plus");
-var checkTag=document.querySelectorAll(".check");
-const btnmintag= document.getElementsByClassName("minus");
+var btndelet = document.getElementsByClassName("delete");
+var btnlike = document.getElementsByClassName("like");
+var checkTag = document.querySelectorAll(".check");
+const btnmintag = document.getElementsByClassName("minus");
 for (var i = 0; i < btnaddtag.length; i++) {
   btnaddtag[i].addEventListener("click", increment);
-  checkTag[i].addEventListener("click" , totalPrice);
-  btnmintag[i].addEventListener("click" , min);
+  checkTag[i].addEventListener("click", totalPrice);
+  btnmintag[i].addEventListener("click", min);
+  btndelet[i].addEventListener("click", delet);
+  btnlike[i].addEventListener("click", like);
 }
 
 function increment(event) {
@@ -26,74 +30,80 @@ function increment(event) {
   var pricetag = trelt.querySelector(".price");
 
   pricevalue = Number(pricetag.innerHTML);
-//   console.log(pricevalue);
+  //   console.log(pricevalue);
 
   var unitpricevalue = parseFloat(trelt.querySelector(".unitPrice").innerHTML);
 
   pricevalue = unitpricevalue * quantityvalue;
-//   console.log(pricevalue);
+  //   console.log(pricevalue);
 
   /*console.log(pricevalue)*/
   pricetag.innerHTML = pricevalue;
 }
 
-function totalPrice(event)
-{
-
-    var checkTag=event.target
-var btnplus=checkTag.parentElement.parentElement.querySelector(".plus")
-var pricevalue =Number(checkTag.parentElement.parentElement.querySelector(".price").innerHTML)
-// console.log(pricevalue)
-var totaltag=document.getElementById("total")
-// console.log(totaltag)
-var totalvalue=Number(totaltag.innerHTML)
-// console.log(totalvalue)
-// totalvalue+=pricevalue
-// totaltag.innerHTML=totalvalue
-if (checkTag.checked===true)
-{
-    totalvalue+=pricevalue
-btnplus.setAttribute("disabled",true)
+function totalPrice(event) {
+  var checkTag = event.target;
+  var btnplus = checkTag.parentElement.parentElement.querySelector(".plus");
+  var btnmin = checkTag.parentElement.parentElement.querySelector(".minus");
+  var pricevalue = Number(
+    checkTag.parentElement.parentElement.querySelector(".price").innerHTML
+  );
+  // console.log(pricevalue)
+  var totaltag = document.getElementById("total");
+  // console.log(totaltag)
+  var totalvalue = Number(totaltag.innerHTML);
+  // console.log(totalvalue)
+  // totalvalue+=pricevalue
+  // totaltag.innerHTML=totalvalue
+  if (checkTag.checked === true) {
+    totalvalue += pricevalue;
+    btnplus.setAttribute("disabled", true);
+    btnmin.setAttribute("disabled", true);
+  } else {
+    totalvalue -= pricevalue;
+    btnplus.removeAttribute("disabled");
+    btnmin.removeAttribute("disabled");
+  }
+  totaltag.innerHTML = totalvalue;
 }
-else
-{
-    totalvalue-=pricevalue
-    btnplus.removeAttribute("disabled")
-}
-totaltag.innerHTML=totalvalue
-}
 
-function min(event)
-{
+function min(event) {
+  var btnmin = event.target;
 
-    var btnmin = event.target;
+  var divelt = btnmin.parentElement;
 
-    var divelt = btnmin.parentElement;
-  
-    var quantitytag = divelt.querySelector("p");
-  
-    var quantityvalue = Number(quantitytag.innerHTML);
-  if (quantityvalue>0){
+  var quantitytag = divelt.querySelector("p");
+
+  var quantityvalue = Number(quantitytag.innerHTML);
+  if (quantityvalue > 0) {
     quantityvalue--;
   }
-    quantitytag.innerHTML = quantityvalue;
-  
-    var trelt = divelt.parentElement.parentElement;
-    // console.log(trelt)
-  
-    var pricetag = trelt.querySelector(".price");
-  
-    pricevalue = Number(pricetag.innerHTML);
-  //   console.log(pricevalue);
-  
-    var unitpricevalue = parseFloat(trelt.querySelector(".unitPrice").innerHTML);
-  
-    pricevalue = unitpricevalue * quantityvalue;
-  //   console.log(pricevalue);
-  
-    /*console.log(pricevalue)*/
-    pricetag.innerHTML = pricevalue;
-    
+  quantitytag.innerHTML = quantityvalue;
 
+  var trelt = divelt.parentElement.parentElement;
+  // console.log(trelt)
+
+  var pricetag = trelt.querySelector(".price");
+
+  pricevalue = Number(pricetag.innerHTML);
+  //   console.log(pricevalue);
+
+  var unitpricevalue = parseFloat(trelt.querySelector(".unitPrice").innerHTML);
+
+  pricevalue = unitpricevalue * quantityvalue;
+  //   console.log(pricevalue);
+
+  /*console.log(pricevalue)*/
+  pricetag.innerHTML = pricevalue;
 }
+function delet(event) {
+  var btndelet = event.target;
 
+  var rowdel = btndelet.parentElement.parentElement.parentElement.parentElement;
+
+  rowdel.remove();
+}
+function like(event) {
+  var btnlike = event.target;
+  btnlike.classList.toggle("toggle");
+}
